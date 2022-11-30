7392ac5176aaf65e6353b7bf25d52ff660257216
Tobyyy <92294076+nabiltoby@users.noreply.github.com>
2022-11-30 10:45:22 +0700

    Add files via upload

---
+++ Pertemuan 2/Coba.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coba : MonoBehaviour{
+    public string myName;
+    // Use this for instalization
+    void Start() {
+        Debug.Log("I am alive and my name is" + myName);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
---
+++ Pertemuan 2/CobaJalan.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CobaJalan : MonoBehaviour {
+    public float movespeed;
+    int status;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //transform.Translate(movespeed, 0, 0); //bisa diganti kode ini
+        transform.position = new Vector3(transform.position.x + movespeed, transform.position.y);
+    }
+}
